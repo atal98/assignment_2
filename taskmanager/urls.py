9a661f93fce39912d79dbf3f66bf1f19a60aec25
atal98 <80337520+atal98@users.noreply.github.com>
2024-06-08 15:40:45 +0530

    add download api output in excel

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import StartScrapingView, ScrapingStatusView
+from .views import StartScrapingView, ScrapingStatusView, ScrapingStatusDownloadView
 
 urlpatterns = [
     path('start_scraping/', StartScrapingView.as_view(), name='start_scraping'),
     path('scraping_status/<str:job_id>/', ScrapingStatusView.as_view(), name='scraping_status'),
+    path('scraping_status_download/<str:job_id>/', ScrapingStatusDownloadView.as_view(), name='scraping_download_status'),
 ]
--- taskmanager/views.py
+++ taskmanager/views.py
@@ -7,6 +7,68 @@ from .task import scrape_coin_data
 import redis
 import json
 from .models import TaskResult
+from pandas.io.json import _normalize
+from io import BytesIO
+import pandas as pd
+from django.http import HttpResponse
+
+def transform_data(json_data):
+    transformed_data = []
+    for task in json_data['tasks']:
+        output = task['output']
+        contracts = output.get('Contracts', [])
+        official_links = output.get('Official links', [])
+        socials = output.get('Socials', [])
+        
+        data = {
+            'job_id': json_data['job_id'],
+            'price': output.get('price', ''),
+            'price_change': output.get('price_change', ''),
+            'market_cap': output.get('market_cap', ''),
+            'volume': output.get('volume', ''),
+            'volume_market_cap': output.get('volume_market_cap', ''),
+            'circulating_supply': output.get('circulating_supply', ''),
+            'total_supply': output.get('total_supply', ''),
+            'max__supply': output.get('max__supply', ''),
+            'fully_diluted_market_cap': output.get('fully_diluted_market_cap', ''),
+            'market_cap_rank': output.get('market_cap_rank', ''),
+            'volume_rank': output.get('volume_rank', ''),
+            'Contracts': ', '.join([f"{contract['name']}: {contract['address']}" for contract in contracts]),
+            'Official_links': ', '.join([f"{link['name']}: {link['url']}" for link in official_links]),
+            'Socials': ', '.join([f"{social['name']}: {social['url']}" for social in socials]),
+            'coin': task.get('coin', '')
+        }
+        
+        transformed_data.append(data)
+    return transformed_data
+
+def json_to_excel(json_data):
+    data = transform_data(json_data)
+    df = pd.DataFrame(data)
+    return dump_to_excel(df, 'task_result')
+
+def dump_to_excel(dataframe, file_name):
+    """Write pandas dataframe to excel sheet.
+
+    Args:
+        dataframe (DataFrame): pandas dataframe
+
+    Returns:
+        bytes: excel sheet bytes
+    """
+    with BytesIO() as bio:
+        writer = pd.ExcelWriter(
+            bio, engine="xlsxwriter", date_format="dd-mm-yyyy"
+        )
+
+        # df_total is the data frame that we want to write into excel.
+        dataframe.reset_index(drop=True, inplace=False)
+        dataframe.index += 1
+        dataframe.to_excel(writer, sheet_name=file_name, index=False)
+        writer.close()
+
+        bio.seek(0)
+        return bio.getvalue()
 
 # Configure Redis connection
 # redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -40,3 +102,20 @@ class ScrapingStatusView(APIView):
         
         # return Response(json.loads(data), status=status.HTTP_200_OK)
         return Response(json.loads(task_result.result), status=status.HTTP_200_OK)
+
+class ScrapingStatusDownloadView(APIView):
+    def get(self, request, job_id):
+        
+        task_result = TaskResult.objects.get(job_id=job_id)
+        data = json.loads(task_result.result)
+
+        if task_result is None:
+            return Response({"error": "Job ID not found or data not available."}, status=status.HTTP_404_NOT_FOUND)
+        
+        workbook = json_to_excel(data)
+        content_type = (
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+        response = HttpResponse(workbook, content_type=content_type)
+        response["Content-Disposition"] = f"attachment; filename=task_result.xlsx"
+        return response
